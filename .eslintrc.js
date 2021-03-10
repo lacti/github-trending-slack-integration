b15e09b5f8286483af8ed2eee696b93452fd4a5a
jaeyoung.choi <lactrious@gmail.com>
2021-03-10 13:24:41 +0900

    Update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+      },
+    ],
+  },
+};
